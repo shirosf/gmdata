1b26654f540d2f3d0980b34512b2af24edcd1772
Shiro Ninomiya <shiro@snino.com>
2020-09-12 22:27:38 +0900

    move to python3

--- ikaren_micread.py
+++ ikaren_micread.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
-import pyaudio
+#!/usr/bin/python3
+#import pyaudio
 import wave
 import sys
 import getopt
 from datetime import datetime
 from threading import Thread
 import shelve
-from twitter_keys import CONSUMER_KEY, CONSUMER_SECRET
-from twitter_oauth_keys import OAUTH_TOKEN, OAUTH_TOKEN_SECRET
-import twitter
+#from twitter_keys import CONSUMER_KEY, CONSUMER_SECRET
+#from twitter_oauth_keys import OAUTH_TOKEN, OAUTH_TOKEN_SECRET
+#import twitter
 import serial
 import socket
 import select
@@ -28,7 +28,7 @@ terminate_program=0
 class UdpConsole():
     wrfd=sys.stdout
     sender=None
-    
+
     def __init__(self, port=udp_port):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -41,9 +41,9 @@ class UdpConsole():
             reads, dws, des = select.select(rlist,[],[])
         except:
             if terminate_program==0:
-                print "error:",sys.exc_info()[0]
+                print("error:",sys.exc_info()[0])
             return 'q'
-            
+
         for rfd in reads:
             if rfd == self.sock:
                 rdata,self.sender=rfd.recvfrom(16)
@@ -81,7 +81,7 @@ class TwitterTweet(Thread):
                 gmdata.hhistory[i*8+5],
                 gmdata.hhistory[i*8+6],
                 gmdata.hhistory[i*8+7])
-        
+
     def create_dmsg(self, gmdata):
         self.msg='last16Days NewToOld:'
         for i in range(2):
@@ -95,7 +95,7 @@ class TwitterTweet(Thread):
                 gmdata.dhistory[i*8+5],
                 gmdata.dhistory[i*8+6],
                 gmdata.dhistory[i*8+7])
-        
+
     def run(self):
         api = twitter.Api(consumer_key=CONSUMER_KEY,
                           consumer_secret=CONSUMER_SECRET,
@@ -105,8 +105,8 @@ class TwitterTweet(Thread):
         status = api.PostUpdate(self.msg)
         udpcon.udpcon_write("%s just posted: %s\n" % (status.user.name, status.text))
 
-        
-    
+
+
 
 class GMDataHistory():
     mhistory=[]
@@ -124,34 +124,34 @@ class GMDataHistory():
 
         self.shelf=shelve.open(HISTORY_DATA_FILE)
 
-        if self.shelf.has_key('dcount'):
+        if 'dcount' in self.shelf:
             self.dcount=self.shelf['dcount']
         else:
             self.dcount=0
 
-        if self.shelf.has_key('hcount'):
+        if 'hcount' in self.shelf:
             self.hcount=self.shelf['hcount']
         else:
             self.hcount=0
-            
-        if self.shelf.has_key('mcount'):
+
+        if 'mcount' in self.shelf:
             self.mcount=self.shelf['mcount']
         else:
             self.mcount=0
-        
-        if self.shelf.has_key('mhistory'):
+
+        if 'mhistory' in self.shelf:
             self.mhistory=self.shelf['mhistory']
         else:
             for i in range(60):
                 self.mhistory.append(0)
-                
-        if self.shelf.has_key('hhistory'):
+
+        if 'hhistory' in self.shelf:
             self.hhistory=self.shelf['hhistory']
         else:
             for i in range(24):
                 self.hhistory.append(0)
-                
-        if self.shelf.has_key('dhistory'):
+
+        if 'dhistory' in self.shelf:
             self.dhistory=self.shelf['dhistory']
         else:
             for i in range(365):
@@ -165,15 +165,15 @@ class GMDataHistory():
         self.shelf['hcount']=self.hcount
         self.shelf['mcount']=self.mcount
         self.shelf.close()
-        
+
     def update_mhistory(self, minute):
         self.lminute=minute
         self.mhistory.pop()
-	self.mcount /= self.mcount_div
+        self.mcount //= self.mcount_div
         self.mhistory.insert(0, self.mcount)
         self.hcount+=self.mcount
         self.mcount=0
-        
+
     def update_hhistory(self, hour):
         self.lhour=hour
         self.hhistory.pop()
@@ -184,7 +184,7 @@ class GMDataHistory():
             tw=TwitterTweet()
             tw.create_msg(self)
             tw.start()
-        
+
     def update_dhistory(self, day):
         self.lday=day
         self.dhistory.pop()
@@ -210,19 +210,19 @@ class GMDataHistory():
 class GMDataSerRead(GMDataHistory):
     def __init__(self, sport, sbaud=9600, twpost=False):
         GMDataHistory.__init__(self)
-	self.twpost=twpost
+        self.twpost=twpost
         self.serial=serial.Serial(port=sport, baudrate=sbaud, timeout=30)
 
     def proc_onechunk(self):
         try:
-            data=self.serial.read(1)
+            data=self.serial.read(1).decode('utf-8')
         except:
-            print "error:",sys.exc_info()[0]
+            print("error:",sys.exc_info()[0])
             return -1
 
         if data != "." and data != "0" and data != "1":
-            print "received garbage character",
-            print data
+            print("received garbage character",)
+            print(data)
             return -1
         self.mcount+=1
         udpcon.udpcon_write("pulse %d\n" % self.mcount)
@@ -231,14 +231,14 @@ class GMDataSerRead(GMDataHistory):
     def close(self):
         GMDataHistory.close(self)
         self.serial.close()
-        
+
 STX=0x02
 ETX=0x03
 ACK=0x06
 class GMDataTCSerRead(GMDataHistory):
     def __init__(self, sport, sbaud=115200, twpost=False):
         GMDataHistory.__init__(self)
-	self.twpost=twpost
+        self.twpost=twpost
         self.serial=serial.Serial(port=sport, baudrate=sbaud, timeout=1)
         self.send_wait_ack('8B0')
         self.send_wait_ack('802')
@@ -282,7 +282,7 @@ class GMDataTCSerRead(GMDataHistory):
         self.send_data('01')
         rd=self.rec_data_tout()
         if rd[0:2]!='01':
-            print "received wrong format, %s" % data
+            print("received wrong format, %s" % data)
             return -1
         x=int(rd[2:], base=16)
         self.send_data('02')
@@ -293,71 +293,18 @@ class GMDataTCSerRead(GMDataHistory):
     def close(self):
         GMDataHistory.close(self)
         self.serial.close()
-        
-    
-class GMDataRead(GMDataHistory):
-    dpcount=0
-    def __init__(self, chunk = 1024, dformat = pyaudio.paInt16,
-                 channels = 1, rate = 44100,
-		 twpost=False, mcount_div=1):
-        GMDataHistory.__init__(self)
-	self.twpost=twpost
-	self.mcount_div=mcount_div
-        self.chunk = chunk
-        self.pa = pyaudio.PyAudio()
-        self.stream = self.pa.open(format = dformat,
-                                   channels = channels,
-                                   rate = rate,
-                                   input = True,
-                                   frames_per_buffer = self.chunk)
 
-    def proc_onechunk(self):
-        data = self.stream.read(self.chunk)
-        for i in range(0,self.chunk*2,2):
-            v=ord(data[i+1])
-            if v>=0x80: v=(~v)&0x7f
-            if v>thr_upper:
-	    	if debug_print:
-		    udpcon.udpcon_write(" +%X" % v)
-                if  self.cont==thr_cont_times:
-		    self.mcount+=1
-		    udpcon.udpcon_write("pulse\n")
-                self.cont+=1
-                self.discont=0
-            elif v<thr_lower:
-	    	if debug_print and self.cont>0:
-		    udpcon.udpcon_write(" -%X" % v)
-                if self.discont>cont_reset:
-                    self.cont=0
-                else:
-                    self.discont+=1
-	    else:
-	    	if debug_print and self.cont>0:
-		    udpcon.udpcon_write(" |%X" % v)
-		self.discont=0
-
-	if debug_print:
-	   udpcon.udpcon_write(" %X" % v)
-	   self.dpcount+=1
-	   if self.dpcount % 16 == 0: udpcon.udpcon_write("\n")
-
-        return 0
-
-    def close(self):
-        GMDataHistory.close(self)
-        self.stream.close()
-        self.pa.terminate()
 
 def usage():
-    print "-t|--tweet: Post to Tweeter"
-    print "-d|--div2: divide count value by 2(for slbook)"
-    print "-p|--debug: print debug messages"
-    print "-u value|--upper=value: upper threshold(default=%d)" % thr_upper
-    print "-l value|--lower=value: lower threshold(default=%d)" % thr_lower
-    print "-c value|--creset=value: reset cont parameter(default=%d)" % cont_reset
-    print "-r value|--ctimes=value: cont times threshold(default=%d)" % thr_cont_times
-    print "-s serial_port|--sport=serial_port: set 'arduino' for (/dev/ttyACM0)"
-    print "-m machine_mode|--mmode=machine_mode: 'pulse:count pulse|tc TC300S/200S"
+    print("-t|--tweet: Post to Tweeter")
+    print("-d|--div2: divide count value by 2(for slbook)")
+    print("-p|--debug: print debug messages")
+    print("-u value|--upper=value: upper threshold(default=%d)" % thr_upper)
+    print("-l value|--lower=value: lower threshold(default=%d)" % thr_lower)
+    print("-c value|--creset=value: reset cont parameter(default=%d)" % cont_reset)
+    print("-r value|--ctimes=value: cont times threshold(default=%d)" % thr_cont_times)
+    print("-s serial_port|--sport=serial_port: set 'arduino' for (/dev/ttyACM0)")
+    print("-m machine_mode|--mmode=machine_mode: 'pulse:count pulse|tc TC300S/200S")
 
 class ReadGmdata(Thread):
     def __init__(self, gmdata, wait_minute):
@@ -374,14 +321,14 @@ class ReadGmdata(Thread):
 
 def signal_handler(signal, frame):
     terminate_program=1
-    print 'got a singal, going to terminate the program'
-    
+    print('got a singal, going to terminate the program')
+
 
 if __name__ == "__main__":
 
     try:
         opts, args = getopt.getopt(sys.argv[1:], "tdpc:u:l:r:s:m:",
-	      ["tweet","div2","debug","creset=","upper=","lower=",
+              ["tweet","div2","debug","creset=","upper=","lower=",
                "ctimes=","sport=","mmode="])
     except getopt.GetoptError:
         usage()
@@ -393,47 +340,44 @@ if __name__ == "__main__":
     mmode='pulse'
     wait_minute=False
     for o, a in opts:
-    	if o in ("-h", "--help"):
-	   usage()
-	   sys.exit()
-    	if o in ("-t", "--tweet"):
-	   print "Tweeet On"
-       	   twpost=True
-    	if o in ("-d", "--div2"):
-	   print "divide count value by 2"
-	   mcount_div=2
-    	if o in ("-p", "--debug"):
-	   debug_print=True
-    	if o in ("-c", "--creset"):
-	   cont_reset=int(a)
-    	if o in ("-u", "--upper"):
-	   thr_upper=int(a)
-    	if o in ("-l", "--lower"):
-	   thr_lower=int(a)
-    	if o in ("-r", "--ctimes"):
-	   thr_cont_times=int(a)
-    	if o in ("-s", "--sport"):
-	   sport=a
+        if o in ("-h", "--help"):
+           usage()
+           sys.exit()
+        if o in ("-t", "--tweet"):
+           print("Tweeet On")
+           twpost=True
+        if o in ("-d", "--div2"):
+           print("divide count value by 2")
+           mcount_div=2
+        if o in ("-p", "--debug"):
+           debug_print=True
+        if o in ("-c", "--creset"):
+           cont_reset=int(a)
+        if o in ("-u", "--upper"):
+           thr_upper=int(a)
+        if o in ("-l", "--lower"):
+           thr_lower=int(a)
+        if o in ("-r", "--ctimes"):
+           thr_cont_times=int(a)
+        if o in ("-s", "--sport"):
+           sport=a
            if sport=="arduino": sport="/dev/ttyACM0"
-           print "Use serial port:%s" % sport
-    	if o in ("-m", "--mmode"):
-	   mmode=a
+           print("Use serial port:%s" % sport)
+        if o in ("-m", "--mmode"):
+           mmode=a
 
     udpcon=UdpConsole();
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
 
-    if sport:
-        if mmode=='pulse':
-            gmdata=GMDataSerRead(sport=sport, twpost=twpost)
-        elif mmode=='tc':
-            gmdata=GMDataTCSerRead(sport=sport, twpost=twpost)
-            wait_minute=True
-        else:
-            usage()
-            sys.exit(2)
+    if mmode=='pulse':
+        gmdata=GMDataSerRead(sport=sport, twpost=twpost)
+    elif mmode=='tc':
+        gmdata=GMDataTCSerRead(sport=sport, twpost=twpost)
+        wait_minute=True
     else:
-        gmdata=GMDataRead(twpost=twpost, mcount_div=mcount_div)
+        usage()
+        sys.exit(2)
 
     read_gmdata=ReadGmdata(gmdata, wait_minute)
     read_gmdata.start()
@@ -474,4 +418,3 @@ if __name__ == "__main__":
     read_gmdata.running=False
     read_gmdata.join()
     gmdata.close()
-
